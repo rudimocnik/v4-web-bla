49fd85258bc0ef9dca5adc35f5a781c1cdadaad3
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-04-12 11:50:02 +0200

    Set up SEO via page-specific HTML tags

--- scripts/generate-entry-points.js
+++ scripts/generate-entry-points.js
@@ -9,9 +9,59 @@ const entryPointsDir = path.resolve(projectRoot, '../entry-points');
 
 const ENTRY_POINTS = [
     {
-        title: 'dYdX',
-        description: 'dYdX',
+        title: 'dYdX Chain | Leading Decentralized Platform for Crypto Perpetual Trading',
+        description: 'Experience the future of decentralized finance with dYdX Chain, the premier platform for secure, transparent, and efficient perpetual trading in the DeFi Space.',
         fileName: 'index.html',
+    },
+    {
+        title: 'dYdX Chain Portfolio | Track Your Orders and Holdings Seamlessly',
+        description: 'Manage and review your portfolio on dYdX Chain with real-time insights, balance updates, and performance analytics for informed trading decisions.',
+        fileName: 'portfolio.html',
+    },
+    {
+        title: 'dYdX Chain Markets | Explore Tradable Pairs and Market Dynamics',
+        description: 'Discover a wide range of cryptocurrency pairs on dYdX Chain. Stay updated with the latest market trends, pricing, and trading opportunities in real-time.',
+        fileName: 'markets.html',
+    },
+    {
+        title: 'dYdX Chain Trading Rewards | Earn $DYDX Tokens Through Active Trading',
+        description: 'Maximize your trading on dYdX Chain and earn DYDX token rewards. Engage with the platform, contribute to the liquidity, and get rewarded.',
+        fileName: 'trading-rewards.html',
+    },
+    {
+        title: 'Trade ETH-USD on dYdX Chain | Ethereum Trading with Deep Liquidity',
+        description: 'Engage in Ethereum trading on dYdX Chain with ETH-USD pair, benefiting from deep liquidity, tight spreads, and advanced trading features.',
+        fileName: 'trade-ETH-USD.html',
+    },
+    {
+        title: 'Trade BTC-USD on dYdX Chain | Bitcoin Trading with Deep Liquidity',
+        description: 'Engage in Bitcoin trading on dYdX Chain with BTC-USD pair, benefiting from deep liquidity, tight spreads, and advanced trading features.',
+        fileName: 'trade-BTC-USD.html',
+    },
+    {
+        title: 'Trade SOL-USD on dYdX Chain | Solana Trading with Deep Liquidity',
+        description: 'Engage in Solana trading on dYdX Chain with SOL-USD pair, benefiting from deep liquidity, tight spreads, and advanced trading features.',
+        fileName: 'trade-SOL-USD.html',
+    },
+    {
+        title: 'Trade AVAX-USD on dYdX Chain | Avalanche Trading with Deep Liquidity',
+        description: 'Engage in Avalanche trading on dYdX Chain with AVAX-USD pair, benefiting from deep liquidity, tight spreads, and advanced trading features.',
+        fileName: 'trade-AVAX-USD.html',
+    },
+    {
+        title: 'Trade LINK-USD on dYdX Chain | Chainlink Trading with Deep Liquidity',
+        description: 'Engage in Chainlink trading on dYdX Chain with LINK-USD pair, benefiting from deep liquidity, tight spreads, and advanced trading features.',
+        fileName: 'trade-LINK-USD.html',
+    },
+    {
+        title: 'Trade MATIC-USD on dYdX Chain | Polygon Trading with Deep Liquidity',
+        description: 'Engage in Polygon trading on dYdX Chain with MATIC-USD pair, benefiting from deep liquidity, tight spreads, and advanced trading features.',
+        fileName: 'trade-MATIC-USD.html',
+    },
+    {
+        title: 'Trade DOGE-USD on dYdX Chain | Dogecoin Trading with Deep Liquidity',
+        description: 'Engage in Dogecoin trading on dYdX Chain with DOGE-USD pair, benefiting from deep liquidity, tight spreads, and advanced trading features.',
+        fileName: 'trade-DOGE-USD.html',
     },
 ];
 
