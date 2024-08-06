79c92686a83c20769f45015e5505dfde6e477d70
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-08-06 12:27:24 +0200

    Add DOS-specific build script

---
+++ scripts/dos-build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Building (DOS)..."
+
+pnpm run build --mode production \
+    && pnpm run build:inject-app-deeplinks \
+    && pnpm run build:inject-analytics \
+    && pnpm run build:inject-intercom \
+    && pnpm run build:inject-statuspage \
+    && pnpm run build:inject-smartbanner
