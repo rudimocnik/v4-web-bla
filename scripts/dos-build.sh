cc077240d2e1b40326896da49d7c7b3e47618314
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-08-06 13:39:25 +0200

    Set up Google Tag Manager

--- scripts/dos-build.sh
+++ scripts/dos-build.sh
@@ -7,4 +7,5 @@ pnpm run build --mode production \
     && pnpm run build:inject-analytics \
     && pnpm run build:inject-intercom \
     && pnpm run build:inject-statuspage \
-    && pnpm run build:inject-smartbanner
+    && pnpm run build:inject-smartbanner \
+    && pnpm run build:inject-google-tag-manager
