8cbc4c5f4ce144ea3c0f849ae70e9a86770e7825
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-09-20 14:11:01 +0200

    Configure dynamic sitemap generation

--- scripts/dos-build.sh
+++ scripts/dos-build.sh
@@ -2,10 +2,14 @@
 
 echo "Building (DOS)..."
 
-pnpm run build --mode production \
-    && pnpm run build:inject-app-deeplinks \
-    && pnpm run build:inject-analytics \
-    && pnpm run build:inject-intercom \
-    && pnpm run build:inject-statuspage \
-    && pnpm run build:inject-smartbanner \
-    && pnpm run build:inject-google-tag-manager
+pnpm run build:set-last-commit-and-tag
+pnpm run build:generate-entry-points
+pnpm run build:generate-sitemap
+tsc
+vite build
+pnpm run build:inject-app-deeplinks
+pnpm run build:inject-analytics
+pnpm run build:inject-intercom
+pnpm run build:inject-statuspage
+pnpm run build:inject-smartbanner
+pnpm run build:inject-google-tag-manager
